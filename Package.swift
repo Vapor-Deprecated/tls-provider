fd1567e6aeac2c70cc9741fb962cbcf6ee7392c8
Logan Wright <logan.william.wright@gmail.com>
2016-06-27 22:02:03 -0400

    updating to latest versions

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporSSL",
     dependencies: [
-        .Package(url: "https://github.com/loganwright/SecretSocks.git", majorVersion: 0, minor: 0),
-        .Package(url: "https://github.com/qutheory/vapor.git", Version(0,0,0))
+        .Package(url: "https://github.com/czechboy0/SecretSocks.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 12)
     ]
 )
