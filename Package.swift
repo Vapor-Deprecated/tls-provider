ed17aad195022b280ee9a36bff9b51361e436f96
Logan Wright <logan.william.wright@gmail.com>
2016-07-18 15:03:17 -0400

    update secret socks

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporTLS",
     dependencies: [
-        .Package(url: "https://github.com/czechboy0/SecretSocks.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/czechboy0/SecretSocks.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 14)
     ]
 )
