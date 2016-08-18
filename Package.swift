bdc28ab6da1b885c9a7fb6f54f968e930653d89e
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-08-19 00:19:19 +0200

    Upping to vapor 0.16

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "VaporTLS",
     dependencies: [
         .Package(url: "https://github.com/czechboy0/SecretSocks.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 15)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 16)
     ]
 )
